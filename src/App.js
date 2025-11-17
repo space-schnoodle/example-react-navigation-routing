93c481333eb6d7d295fc66592724f37939737d8a
Tatiana C. Molina Higgins <73068033+space-schnoodle@users.noreply.github.com>
2025-11-17 15:01:34 -0500

    add accordion component
    add accordion component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
-import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
-import Button from './Button';
+import Accordion from './components/Accordion';
 
 function App() {
+  const items = [
+    {
+      id: '4bhdgfk',
+      label: 'Can I use React on a project?',
+      content: 'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.'
+    },
+    {
+      id: 'sdsdfsd',
+      label: 'Can I use Javascript on a project?',
+      content: 'You can use Javascript on any project you want. You can use Javascript on any project you want. You can use Javascript on any project you want. You can use Javascript on any project you want. You can use Javascript on any project you want.' 
+    },
+    {
+      id: '46539jd',
+      label: 'Can I use CSS on a project?',
+      content: 'You can use CSS on any project you want. You can use CSS on any project you want. You can use CSS on any project you want. You can use CSS on any project you want. You can use CSS on any project you want.'
+    }
+  ];
+
   return (
     <div>
-      <div>
-        <Button primary className="mb-5">
-          <GoBell />
-          Click me!!
-        </Button>
-      </div>
-      <div>
-        <Button secondary>
-          <GoCloudDownload/>
-          Buy Now!
-        </Button>
-      </div>
-      <div>
-        <Button danger>
-          <GoDatabase/>
-          Hide Ads!
-        </Button>
-      </div>
-      <div>
-        <Button warning>Save The World!</Button>
-      </div>
-      <div>
-        <Button success>Make peace!</Button>
-      </div>
+      <Accordion items={items}/>
     </div>    
   );
 };
---
+++ src/pages/ButtonPage.js
@@ -0,0 +1,35 @@
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
+import Button from '../components/Button';
+
+function ButtonPage() {
+  return (
+    <div>
+      <div>
+        <Button primary className="mb-5">
+          <GoBell />
+          Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button secondary>
+          <GoCloudDownload/>
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button danger>
+          <GoDatabase/>
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button warning>Save The World!</Button>
+      </div>
+      <div>
+        <Button success>Make peace!</Button>
+      </div>
+    </div>    
+  );
+};
+
+export default ButtonPage;
